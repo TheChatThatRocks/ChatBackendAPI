ef5ae89d9ca2cafd0912f8b40f82b95ae700b93a
AbelChT <abelcht@outlook.es>
2020-04-09 14:05:06 +0200

    New services definition

--- src/main/java/com/eina/chat/backendapi/service/GroupsManagmentDatabaseAPI.java
+++ src/main/java/com/eina/chat/backendapi/service/GroupsManagmentDatabaseAPI.java
@@ -3,6 +3,6 @@ package com.eina.chat.backendapi.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MessageHistoryDatabaseAPI {
+public class GroupsManagmentDatabaseAPI {
     // TODO:
 }
