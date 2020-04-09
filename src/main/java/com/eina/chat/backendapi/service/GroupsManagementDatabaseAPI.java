737892c003b214d3f7ab6543a5acc48ca2e28535
AbelChT <abelcht@outlook.es>
2020-04-09 14:05:54 +0200

    name solved

--- src/main/java/com/eina/chat/backendapi/service/GroupsManagementDatabaseAPI.java
+++ src/main/java/com/eina/chat/backendapi/service/GroupsManagementDatabaseAPI.java
@@ -3,6 +3,6 @@ package com.eina.chat.backendapi.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GroupsManagmentDatabaseAPI {
+public class GroupsManagementDatabaseAPI {
     // TODO:
 }
